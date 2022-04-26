0300bfc6966cdc9f222021f0b464f25b9efae5a4
f0leg <foleg6320@gmail.com>
2022-04-26 20:33:01 +0300

    Create new function

--- sandbox/src/main/java/ru/foleg/sandbox/MySecond.java
+++ sandbox/src/main/java/ru/foleg/sandbox/MySecond.java
@@ -3,7 +3,27 @@ package ru.foleg.sandbox;
 public class MySecond {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("User");
+
+		double len = 5;
+		System.out.println("Площадь " + len + " = " + area(len));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Gkjofnm ghzvjeujkmybrf cj сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l) {
+		return l * l;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 
 }
